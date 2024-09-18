9d610639347ef6f1db7c0dd5c1e0cc35dd2946d2
BrunoMatheus10 <brunomatheus88300432@gmail.com>
2024-09-18 19:41:58 -0300

    Update atividade-tema12-10.py

--- atividade-tema12-10.py
+++ atividade-tema12-10.py
@@ -12,9 +12,9 @@ while numero < 1 or numero > 10:
 numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 # Exibe a tabuada do número digitado usando o laço de repetição PARA (for)
-print(f"Tabuada do {numero}:")
+print("Tabuada do ",numero)
 # Laço de repetição PARA (for) para iterar sobre cada número na lista 'numeros'
 for vez in numeros:
     # Calcula o resultado da multiplicação do número digitado pelo usuário com o valor atual de 'vez'
     # Exibe o resultado da multiplicação no formato "numero x vez = resultado"
-    print(f"{numero} x {vez} = {numero * vez}")
+    print(numero," x " ,vez," = ",numero * vez)
